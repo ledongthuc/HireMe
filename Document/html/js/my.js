53d119b2ede4a956f96ac7ea575504ca25500ccd
ledongthuc <thucdongle@yahoo.com>
2013-08-13 07:48:12 +0700

    Init project (Home, List work)

---
+++ Document/html/js/my.js
@@ -0,0 +1,14 @@
+/**
+ * @author Thuc
+ */
+$("#category-navigation").mmenu({
+   searchfield   : {
+      add           : true,
+      search        : true,
+      placeholder   : "Categories search",
+      noResults     : "No results found.",
+      showLinksOnly : true
+   },
+   position: "left",
+});
+
